f5cc15a72d726d6837c42e392e60311fb2c00ed5
melaniestorr <storr@wortbildton.de>
2021-09-16 22:17:33 +0200

    fix import typo

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 const useFetch = (hexcode) => {
   const [grayvalue, setGrayvalue] = useState(hexcode);
